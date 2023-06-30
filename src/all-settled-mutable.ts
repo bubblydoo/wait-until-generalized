3e4ab210b08c9b1ced233005d179847834169c60
Hans Otto Wirtz <hansottowirtz@gmail.com>
2023-06-30 10:31:31 +0200

    feat: add cleaner implementation

--- src/all-settled-mutable.ts
+++ src/all-settled-mutable.ts
@@ -1,12 +1,10 @@
 /** Like `Promise.allSettled`, but handles modifications to the promises array */
 export async function allSettledMutable(promises: Promise<unknown>[]): Promise<PromiseSettledResult<unknown>[]> {
-  let len = 0;
-  let values: unknown[] = []; // actually Promise[]
-  // when the length of the array changes, there has been a nested call to waitUntil
-  // and we should await the promises again
-  while (len !== promises.length) {
-    len = promises.length;
-    values = (await Promise.allSettled(promises)) as any;
-  }
-  return values as any;
+	let values: PromiseSettledResult<unknown>[]
+	// when the length of the array changes, there has been a nested call to waitUntil
+	// and we should await the promises again
+	do {
+		values = await Promise.allSettled(promises)
+	} while (values.length !== promises.length)
+	return values
 }
